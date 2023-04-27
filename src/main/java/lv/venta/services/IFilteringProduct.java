fa5e0cb4c5ced28390cc955791916351a6654f04
Kristers Cipkins <s20cipkkris@vea.lv>
2023-04-27 11:21:55 +0300

    add new filtering service as interface

---
+++ src/main/java/lv/venta/services/IFilteringProduct.java
@@ -0,0 +1,20 @@
+package lv.venta.services;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface IFilteringProduct {
+	
+	//filter product: price less than X
+	public abstract ArrayList<Product> filterByPriceLessThan(float priceThreshold)  throws Exception;
+	
+	//filter product: quantity less than X
+	public abstract ArrayList<Product> quantityLessThan(int quantity)  throws Exception;
+	
+	//filter product: sorting
+	public abstract ArrayList<Product> sort();
+	
+	
+	
+}
