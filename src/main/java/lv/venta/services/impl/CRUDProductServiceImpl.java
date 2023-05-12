eee8dfe056433b8c0deb426c24b7431160958eb6
localuser <localuser@VEA.LV>
2023-05-12 15:56:47 +0300

    Changed service related to repo

--- src/main/java/lv/venta/services/impl/CRUDProductServiceImpl.java
+++ src/main/java/lv/venta/services/impl/CRUDProductServiceImpl.java
@@ -13,84 +13,76 @@ import lv.venta.services.ICRUDProductService;
 @Service
 public class CRUDProductServiceImpl implements ICRUDProductService {
 
-	@Autowired
-	private IProductRepo productRepo;
+  @Autowired
+  private IProductRepo productRepo;
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------  
-	@Override
-	public void addNewProduct(String title, String description, float price, int quantity) throws Exception {
-		if (productRepo.exexistsByTitleAndDescriptionAndPrice(title, description, price)) {
-			Product product = productRepo.finfindByTitleAndDescriptionAndPrice(title, description, price);
-			product.setQuantity(product.getQuantity() + quantity);
-			productRepo.save(product);
-		} else {
-			Product newProduct = new Product(title, description, price, quantity);
-			productRepo.save(newProduct);
-		}
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------  
+  @Override
+  public void addNewProduct(String title, String description, float price, int quantity) throws Exception {
+    if (productRepo.exexistsByTitleAndDescriptionAndPrice(title, description, price)) {
+      Product product = productRepo.finfindByTitleAndDescriptionAndPrice(title, description, price);
+      product.setQuantity(product.getQuantity() + quantity);
+      productRepo.save(product);
+    } else {
+      Product newProduct = new Product(title, description, price, quantity);
+      productRepo.save(newProduct);
+    }
 
-	}
+  }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-	@Override
-	public ArrayList<Product> retriveAllProducts() {
-		return (ArrayList<Product>) productRepo.findAll();
-	}
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  @Override
+  public ArrayList < Product > retriveAllProducts() {
+    return (ArrayList < Product > ) productRepo.findAll();
+  }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-	@Override
-	public Product retriveProductById(long id) throws Exception {
-		if (id > 0) {
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  @Override
+  public Product retriveProductById(long id) throws Exception {
+    if (id > 0) {
 
-			if (productRepo.existsById(id)) {
-				Product temp = productRepo.findById(id).get();
-				return temp;
-			} else {
-				throw new Exception("There is no product with this Id");
-			}
+      if (productRepo.existsById(id)) {
+        Product temp = productRepo.findById(id).get();
+        return temp;
+      } else {
+        throw new Exception("There is no product with this Id");
+      }
 
-		} else {
-			throw new Exception("Incorrect Id needs to be pozitive");
-		}
-	}
+    } else {
+      throw new Exception("Incorrect Id needs to be pozitive");
+    }
+  }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-	@Override
-	public void updateProductById(long id, String title, String description, float price, int quantity)
-			throws Exception {
-		if (id > 0) {
-			if (title != null && description != null && price > 0 && price < 10000 && quantity > 0
-					&& quantity < 100000) {
-				boolean isFound = false;
-				for (Product product : allProductList) {
-					if (product.getId() == id) {
-						product.setTitle(title);
-						product.setDescription(description);
-						product.setPrice(price);
-						product.setQuantity(quantity);
-						break;
-					}
-				}
-				if (isFound) {
-					throw new Exception("No product with this id");
-				}
-			} else {
-				throw new Exception("incorrect params");
-			}
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  @Override
+  public void updateProductById(long id, String title, String description, float price, int quantity) throws Exception {
+    if (id > 0) {
 
-		} else {
-			throw new Exception("ID needs to be positive");
-		}
+      if (productRepo.existsById(id)) {
+        Product product = productRepo.findById(id).get();
+        product.setTitle(title);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setQuantity(quantity);
+        productRepo.save(product);
+      } else {
+        throw new Exception("No product with this id");
+      }
 
-	}
+    } else {
+      throw new Exception("ID needs to be positive");
+    }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-	@Override
-	public void deleteProductById(long id) throws Exception {
-		if (productRepo.existsById(id)) {
-			productRepo.deleteById(id);
-		} else {
-			throw new Exception("There is no product with this Id");
-		}
-	}
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+  @Override
+  public void deleteProductById(long id) throws Exception {
+    if (productRepo.existsById(id)) {
+      productRepo.deleteById(id);
+    } else {
+      throw new Exception("There is no product with this Id");
+    }
+  }
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 }
